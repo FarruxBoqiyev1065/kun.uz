6cc8a72ac19f896982ace48aa8c41de1d3f3acf6
Farrux <fboqiyev1065@gmail.com>
2026-05-19 02:13:12 +0500

    smth

--- src/main/java/dasturlash/uz/entities/ProfileRoleEntity.java
+++ src/main/java/dasturlash/uz/entities/ProfileRoleEntity.java
@@ -1,5 +1,6 @@
 package dasturlash.uz.entities;
 
+import dasturlash.uz.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,14 +13,12 @@ public class ProfileRoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @Column(name = "profile_id")
     private Integer profileId;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "profile_id", insertable = false, updatable = false, nullable = false)
     private ProfileEntity profile;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "roles")
-    private ProfileRoleEnum roles;
+    private Role roles;
 }
