9c36d0ff18bdbc2b1e6a57b491526aa6db9cdee1
Farrux <fboqiyev1065@gmail.com>
2026-05-18 19:43:49 +0500

    RegionEntity's fields are changed

--- src/main/java/dasturlash/uz/entities/RegionEntity.java
+++ src/main/java/dasturlash/uz/entities/RegionEntity.java
@@ -3,6 +3,7 @@ package dasturlash.uz.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -14,18 +15,26 @@ public class RegionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column
-    private Integer order_number;
-    @Column
+
+    @Column(name = "order_number")
+    private Integer orderNumber;
+
+    @Column(name = "name_uz")
     private String nameUz;
-    @Column
+
+    @Column(name = "name_ru")
     private String nameRu;
-    @Column
+
+    @Column(name = "name_en")
     private String nameEn;
-    @Column
-    private Boolean visible = Boolean.TRUE;
-    @Column
-    private String key;
-    @Column
+
+    @Column(name = "region_key")
+    private String regionKey;
+
+    @Column(name = "visible")
+    private Boolean visible = true;
+
+    @Column(name = "created_date")
+    @CreationTimestamp
     private LocalDateTime createdDate;
 }
