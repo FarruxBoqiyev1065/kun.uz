6eecb691cf219ec01f9598192656a7963121b766
Farrux <fboqiyev1065@gmail.com>
2026-05-19 15:55:15 +0500

    findByIdAndVisibleIsTrue method created

--- src/main/java/dasturlash/uz/reposiroty/ProfileRepository.java
+++ src/main/java/dasturlash/uz/reposiroty/ProfileRepository.java
@@ -4,7 +4,11 @@ import dasturlash.uz.entities.ProfileEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends CrudRepository<ProfileEntity, Integer> {
-
+    Optional<ProfileEntity> findByUsernameAndVisibleIsTrue(String username);
+    Optional<ProfileEntity> findByIdAndVisibleIsTrue(Integer id);
 }
