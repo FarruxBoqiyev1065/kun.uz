d0940f95828dfa50fa7e4a64691de9fca71d7b72
Farrux <fboqiyev1065@gmail.com>
2026-05-19 15:56:48 +0500

    Profile created with create method and getById method added

--- src/main/java/dasturlash/uz/service/ProfileService.java
+++ src/main/java/dasturlash/uz/service/ProfileService.java
@@ -2,31 +2,64 @@ package dasturlash.uz.service;
 
 import dasturlash.uz.dto.ProfileDto;
 import dasturlash.uz.entities.ProfileEntity;
-import dasturlash.uz.enums.Role;
 import dasturlash.uz.enums.Status;
+import dasturlash.uz.exceptions.BadRequestException;
 import dasturlash.uz.reposiroty.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class ProfileService {
     @Autowired
     private ProfileRepository repository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private ProfileRoleService profileRoleService;
 
     public ProfileDto create(ProfileDto dto) {
+        Optional<ProfileEntity> optional = repository.findByUsernameAndVisibleIsTrue(dto.getUsername());
+        if (optional.isPresent()) {
+            throw new BadRequestException("Username exists");
+        }
+
         ProfileEntity entity = new ProfileEntity();
         entity.setName(dto.getName());
         entity.setSurname(dto.getSurname());
-        entity.setPhone(dto.getPhone());
-        entity.setEmail(dto.getEmail());
-        entity.setPassword(dto.getPassword());
-        entity.setStatus(Status.CREATED);
-        entity.setRole(Role.USER);
-        entity.setCreatedDate(LocalDateTime.now());
+        entity.setUsername(dto.getUsername());
+        entity.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
+        entity.setVisible(Boolean.TRUE);
+        entity.setStatus(Status.ACTIVE);
         repository.save(entity);
+        // add user roles
+        profileRoleService.merge(entity.getId(), dto.getRoleList());
+
+        ProfileDto response = toDTO(entity);
+        return response;
+    }
+
+    public ProfileDto toDTO(ProfileEntity entity) {
+        ProfileDto dto = new ProfileDto();
         dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setSurname(entity.getSurname());
+        dto.setUsername(entity.getUsername());
+        dto.setCreatedDate(entity.getCreatedDate());
         return dto;
+    }
+
+    public ProfileDto getById(Integer id) {
+        ProfileEntity entity = get(id);
+        ProfileDto dto = toDTO(entity);
+        dto.setRoleList(profileRoleService.getByProfileId(id));
+        return dto;
+    }
+
+    private ProfileEntity get(Integer id) {
+        return repository.findByIdAndVisibleIsTrue(id).orElseThrow(
+                () -> new BadRequestException("Profile not found"));
     }
 }
