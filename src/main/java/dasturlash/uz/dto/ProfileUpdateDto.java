30bfa2f02dfa0bf3415e8d8e59f965477608cbc3
Farrux <fboqiyev1065@gmail.com>
2026-05-19 16:35:18 +0500

    ProfileUpdateDto added for update profile without password

---
+++ src/main/java/dasturlash/uz/dto/ProfileUpdateDto.java
@@ -0,0 +1,25 @@
+package dasturlash.uz.dto;
+
+import dasturlash.uz.enums.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ProfileUpdateDto {
+    @NotBlank(message = "Ism bo‘sh bo‘lmasligi kerak")
+    private String name;
+
+    @NotBlank(message = "Familiya bo‘sh bo‘lmasligi kerak")
+    private String surname;
+
+    @NotBlank(message = "Username  bo‘sh bo‘lmasligi kerak")
+    private String username;
+
+    @NotEmpty(message = "Role bo‘sh bo‘lmasligi kerak")
+    private List<Role> roleList;
+}
