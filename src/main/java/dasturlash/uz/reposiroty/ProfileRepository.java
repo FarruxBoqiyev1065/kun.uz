995688c91bbc7fe5b04a2823ed59039fbd81275e
Farrux <fboqiyev1065@gmail.com>
2026-05-19 18:17:10 +0500

    Repository get extends PagingAndSorting, and pagination query written

--- src/main/java/dasturlash/uz/reposiroty/ProfileRepository.java
+++ src/main/java/dasturlash/uz/reposiroty/ProfileRepository.java
@@ -1,14 +1,22 @@
 package dasturlash.uz.reposiroty;
 
 import dasturlash.uz.entities.ProfileEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProfileRepository extends CrudRepository<ProfileEntity, Integer> {
+public interface ProfileRepository extends CrudRepository<ProfileEntity, Integer>, PagingAndSortingRepository<ProfileEntity, Integer> {
     Optional<ProfileEntity> findByUsernameAndVisibleIsTrue(String username);
+
     Optional<ProfileEntity> findByIdAndVisibleIsTrue(Integer id);
+
+    @Query("From ProfileEntity p inner join fetch p.roleList where p.visible = true")
+    Page<ProfileEntity> findAllWithRoles(Pageable pageable);
 }
