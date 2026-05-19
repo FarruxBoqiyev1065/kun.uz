ac62549c4efada654f796e53482261d17adcbc43
Farrux <fboqiyev1065@gmail.com>
2026-05-19 15:51:06 +0500

    getById API added

--- src/main/java/dasturlash/uz/controller/ProfileController.java
+++ src/main/java/dasturlash/uz/controller/ProfileController.java
@@ -2,22 +2,25 @@ package dasturlash.uz.controller;
 
 import dasturlash.uz.dto.ProfileDto;
 import dasturlash.uz.service.ProfileService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/profile")
+@RequestMapping("/api/v1/profile")
 public class ProfileController {
     @Autowired
     private ProfileService service;
 
     @PostMapping("")
-    public ResponseEntity<ProfileDto> create(@RequestBody ProfileDto dto){
+    public ResponseEntity<ProfileDto> create(@Valid @RequestBody ProfileDto dto){
         ProfileDto result = service.create(dto);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfileDto> getById(@PathVariable Integer id){
+        return ResponseEntity.ok(service.getById(id));
     }
 }
