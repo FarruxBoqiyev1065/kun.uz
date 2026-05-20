faeee927f4df99f01552d8f0669a0b25f3c83f99
Farrux <fboqiyev1065@gmail.com>
2026-05-21 00:51:04 +0500

    All queries ready for service

--- src/main/java/dasturlash/uz/reposiroty/SectionRepository.java
+++ src/main/java/dasturlash/uz/reposiroty/SectionRepository.java
@@ -1,4 +1,37 @@
 package dasturlash.uz.reposiroty;
 
-public interface SectionRepository {
+import dasturlash.uz.entities.SectionEntity;
+import dasturlash.uz.mapper.SectionMapper;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SectionRepository extends CrudRepository<SectionEntity, Integer> {
+    Optional<SectionEntity> findByIdAndVisibleIsTrue(Integer id);
+    Optional<SectionEntity> findBySectionKey(String key);
+
+    @Transactional
+    @Modifying
+    @Query("update SectionEntity set visible = false where id = ?1")
+    int updateVisibleById(Integer id);
+
+    @Query("SELECT c.id AS id, " +
+            "CASE :lang " +
+            "   WHEN 'UZ' THEN c.nameUz " +
+            "   WHEN 'RU' THEN c.nameRu " +
+            "   WHEN 'EN' THEN c.nameEn " +
+            "END AS name, " +
+            "c.orderNumber AS orderNumber, " +
+            "c.sectionKey AS sectionKey " +
+            "FROM SectionEntity c " +
+            "WHERE c.visible = true order by orderNumber asc")
+    List<SectionMapper> getByLang(@Param("lang") String lang);
+
 }
