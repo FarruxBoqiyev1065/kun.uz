c15f0dd76a4d5d6e253c24bca209059010e844dc
Farrux <fboqiyev1065@gmail.com>
2026-05-20 19:53:25 +0500

    Queries created for crude service

--- src/main/java/dasturlash/uz/reposiroty/CategoryRepository.java
+++ src/main/java/dasturlash/uz/reposiroty/CategoryRepository.java
@@ -1,4 +1,21 @@
 package dasturlash.uz.reposiroty;
 
-public interface CategoryRepository {
+import dasturlash.uz.entities.CategoryEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository  extends CrudRepository<CategoryEntity, Integer> {
+    Optional<CategoryEntity> findByIdAndVisibleIsTrue(Integer id);
+    Optional<CategoryEntity> findByCategoryKey(String key);
+
+    @Transactional
+    @Modifying
+    @Query("update CategoryEntity set visible = false where id = ?1")
+    int updateVisibleById(Integer id);
 }
