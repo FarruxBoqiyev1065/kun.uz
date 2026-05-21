8f490f1fe0f6c806babb91a2be1a5efe2757e5d3
Farrux <fboqiyev1065@gmail.com>
2026-05-21 19:22:59 +0500

    Queries written

---
+++ src/main/java/dasturlash/uz/reposiroty/EmailHistoryRepository.java
@@ -0,0 +1,17 @@
+package dasturlash.uz.reposiroty;
+
+import dasturlash.uz.entities.EmailHistoryEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface EmailHistoryRepository extends CrudRepository<EmailHistoryEntity, String> {
+
+
+    Optional<EmailHistoryEntity> findTopByToAccountOrderByCreatedDateDesc(String account);
+
+    @Query("from EmailHistoryEntity where toAccount = ?1 order by createdDate desc limit 1")
+    Optional<EmailHistoryEntity> findLastByAccount(String account);
+
+}
