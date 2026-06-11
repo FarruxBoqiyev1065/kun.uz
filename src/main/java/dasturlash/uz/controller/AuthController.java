1161d19e07f654ed2835443ffbf211c8b91041e0
Farrux <fboqiyev1065@gmail.com>
2026-06-11 20:37:12 +0500

    Email get method added

--- src/main/java/dasturlash/uz/controller/AuthController.java
+++ src/main/java/dasturlash/uz/controller/AuthController.java
@@ -7,10 +7,7 @@ import dasturlash.uz.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -27,4 +24,14 @@ public class AuthController {
     public ResponseEntity<ProfileDto> login(@Valid @RequestBody AuthDto dto){
         return ResponseEntity.ok(service.login(dto));
     }
+
+    @GetMapping("/registration/email/verification/{username}/{smsCode}")
+    public ResponseEntity<String> registration(@PathVariable("username") String username, @PathVariable("smsCode") Integer smsCode) {
+        return ResponseEntity.ok(service.regEmailVerification(username, smsCode));
+    }
+
+//    @GetMapping("/registration//resend")
+//    public ResponseEntity<String> registration(@RequestBody RegistrationResendDto dto){
+//        return ResponseEntity.ok(service.regResend(dto));
+//    }
 }
