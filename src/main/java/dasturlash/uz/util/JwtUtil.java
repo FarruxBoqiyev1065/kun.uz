00bd0c084c5dcfdd73ce3c92374e33377406bf0e
Farrux <fboqiyev1065@gmail.com>
2026-06-11 23:31:46 +0500

    JwtUtil added for get and give jwt to users

---
+++ src/main/java/dasturlash/uz/util/JwtUtil.java
@@ -0,0 +1,48 @@
+package dasturlash.uz.util;
+
+import dasturlash.uz.dto.Jwt.JwtDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+    private static final int tokenLiveTime = 1000 * 3600 * 24; // 1-day
+    private static final String secretKey = "veryLongSecretmazgillattayevlasharaaxmojonjinnijonsurbetbekkiydirhonuxlatdibekloxovdangasabekochkozjonduxovmashaynikmaydagapchishularnioqiganbolsangizgapyoqaniqsizmazgi";
+
+    public static String encode(String username, String role) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("role", role);
+
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + tokenLiveTime))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    public static JwtDto decode(String token) {
+        Claims claims = Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        String username = (String) claims.get("username");
+        String role = (String) claims.get("role");
+        return new JwtDto(username, role);
+    }
+
+    private static SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
